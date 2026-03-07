f46b2fc1473448d9843d82f3af6e75850971a211
Sebastián galvis <galviss131@outlook.com>
2026-03-07 18:57:10 -0500

    Saved

---
+++ Workshop3/Chess/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese ubicación de los caballos: ");
+        string entrada = Console.ReadLine();
+
+        string[] caballos = entrada.Split(',');
+
+        Dictionary<string, (int x, int y)> posiciones = new Dictionary<string, (int, int)>();
+
+        foreach (string c in caballos)
+        {
+            int x = char.ToUpper(c[0]) - 'A';
+            int y = int.Parse(c[1].ToString()) - 1;
+
+            posiciones[c.ToUpper()] = (x, y);
+        }
+
+        int[][] movimientos = new int[][]
+        {
+            new int[] {2,1},
+            new int[] {2,-1},
+            new int[] {-2,1},
+            new int[] {-2,-1},
+            new int[] {1,2},
+            new int[] {1,-2},
+            new int[] {-1,2},
+            new int[] {-1,-2}
+        };
+
+        foreach (var caballo in posiciones)
+        {
+            List<string> conflictos = new List<string>();
+
+            foreach (var otro in posiciones)
+            {
+                if (caballo.Key == otro.Key)
+                    continue;
+
+                foreach (var m in movimientos)
+                {
+                    int nx = caballo.Value.x + m[0];
+                    int ny = caballo.Value.y + m[1];
+
+                    if (nx == otro.Value.x && ny == otro.Value.y)
+                    {
+                        conflictos.Add(otro.Key);
+                    }
+                }
+            }
+
+            if (conflictos.Count == 0)
+            {
+                Console.WriteLine($"Analizando Caballo en {caballo.Key} => Conflicto con ninguno");
+            }
+            else
+            {
+                Console.WriteLine($"Analizando Caballo en {caballo.Key} => Conflicto con {string.Join(", ", conflictos)}");
+            }
+        }
+    }
+}
---
+++ Workshop3/TheBeam/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la viga: ");
+        string viga = Console.ReadLine();
+
+        char baseViga = viga[0];
+
+        int resistencia = 0;
+
+        switch (baseViga)
+        {
+            case '%': resistencia = 10; break;
+            case '&': resistencia = 30; break;
+            case '#': resistencia = 90; break;
+            default:
+                Console.WriteLine("Base inválida");
+                return;
+        }
+
+        int pesoTotal = 0;
+        int secuenciaLargueros = 0;
+        int conexionesSeguidas = 0;
+
+        for (int i = 1; i < viga.Length; i++)
+        {
+            char c = viga[i];
+
+            if (c == '=')
+            {
+                secuenciaLargueros++;
+                conexionesSeguidas = 0;
+                pesoTotal += secuenciaLargueros;
+            }
+            else if (c == '*')
+            {
+                conexionesSeguidas++;
+
+                if (conexionesSeguidas > 2)
+                {
+                    Console.WriteLine("La viga está mal construida!");
+                    return;
+                }
+
+                pesoTotal += secuenciaLargueros * 2;
+            }
+            else
+            {
+                Console.WriteLine("Carácter inválido");
+                return;
+            }
+        }
+
+        if (pesoTotal <= resistencia)
+            Console.WriteLine("La viga soporta el peso!");
+        else
+            Console.WriteLine("La viga NO soporta el peso!");
+    }
+}
